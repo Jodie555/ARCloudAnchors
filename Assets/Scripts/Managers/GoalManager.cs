9b6bbc4e9b05db80dcb995a46b4edbd2d7ebd12f
Jodie <aps19isec@gmail.com>
2024-10-01 14:05:05 +0100

    Successfully created the main Scene (ARDemoScene)

---
+++ Assets/Scripts/Managers/GoalManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour
+{
+
+    [Tooltip("The greeting prompt Game Object to show when onboarding begins.")]
+    [SerializeField]
+    GameObject m_GreetingPrompt;
+
+    /// <summary>
+    /// The greeting prompt Game Object to show when onboarding begins.
+    /// </summary>
+    public GameObject greetingPrompt
+    {
+        get => m_GreetingPrompt;
+        set => m_GreetingPrompt = value;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CompleteGoal()
+    {
+        m_GreetingPrompt.SetActive(false);
+
+    }
+}
